06bde74b67f50d895c93cd4ff85ecd0b22aaf1b2
Marco Gaia <msa@lionx.com.br>
2022-01-31 18:04:01 -0300

    feat: deploy on fission

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 fission spec init
-fission env create --spec --name get-user-ticket-details-env --image fission/python-env-3.8 --builder fission/python-builder-3.8
+fission env create --spec --name get-user-ticket-details-env --image nexus.sigame.com.br/python-env-3.8:0.0.4 --builder nexus.sigame.com.br/python-builder-3.8:0.0.1
 fission fn create --spec --name get-user-ticket-details-fn --env get-user-ticket-details-env --src "./func/*" --entrypoint main.fn
-fission route create --spec --method POST --url /post_user_ticket --function get-user-ticket-details-fn
+fission route create --spec --method GET --url /get_user_ticket_details --function get-user-ticket-details-fn
